81712a1ada6309b5233b1492ac1410f66c648ce5
adelwael982 <adelwael2006@gmail.com>
2026-08-02 17:48:53 +0300

    Add files via upload

---
+++ Semi Project Driver/README.md.txt
@@ -0,0 +1,3 @@
+The driver is an important component in many designs it simply chooses to allow the data to pass through or not 
+I made it using a continuous assignment 
+ 
---
+++ Semi Project Driver/driver.v
@@ -0,0 +1,9 @@
+module driver #(
+    parameter WIDTH=8
+) (
+    input [WIDTH-1:0] data_in,
+    input data_en,
+    output [WIDTH-1:0] data_out
+);
+    assign data_out= data_en ? data_in:'bz;
+endmodule
