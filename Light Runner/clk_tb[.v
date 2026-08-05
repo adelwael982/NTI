75979e7031dcdfe5d7271994daa52e19be97e1ba
Adel Wael <adelwael2006@gmail.com>
2026-08-05 18:57:11 +0300

    Update clk_tb[.v

--- Light Runner/clk_tb[.v
+++ Light Runner/clk_tb[.v
@@ -3,7 +3,7 @@ module light_shifter_tb;
 reg clk;
 reg rst_n;
 reg hold;
-wire s;
+wire [3:0]s;
 
 light_shifter c1(
     .clk(clk),
@@ -13,12 +13,14 @@ light_shifter c1(
 );
 always #10 clk=~clk;
 initial begin
-    clk=0;rst_n=1;
-    #1;
-    rst_n=0;
-    #1;
+    clk=0;rst_n=0;hold=1;
+    #20;
     rst_n=1;
-    #2000000002;
+    #1000000000;
+    hold=0;
+    #1000000000;
+    hold=1;
+   #4000000000;
     $stop;
 end
 endmodule
